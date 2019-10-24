e7508a8d34b0a408252e2730371677a8d094d8f0
robben5566 <ccav31104@gmail.com>
2019-10-24 13:58:53 +0800

    add home c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseLayoutComponent
+    BaseLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
