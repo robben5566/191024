7d2247547f8c11aec20f6f47001f3458ae9ebd10
robben5566 <ccav31104@gmail.com>
2019-10-24 14:56:38 +0800

    use const variables store component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { BaseLayoutComponent } from "./layouts/base-layout/base-layout.component";
 import { RouterModule } from "@angular/router";
 
+const COMPONENTS = [BaseLayoutComponent];
 @NgModule({
-  declarations: [BaseLayoutComponent],
+  declarations: [...COMPONENTS],
   imports: [CommonModule, RouterModule],
-  exports: [BaseLayoutComponent]
+  exports: [...COMPONENTS]
 })
 export class SharedModule {}
