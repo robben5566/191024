e2fe1efce1f45718955d236f8255fc1c8f7e14b2
robben5566 <ccav31104@gmail.com>
2019-10-24 15:08:25 +0800

    rename

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from "@angular/common";
 import { PagesRoutingModule } from "./pages-routing.module";
 import { HomeComponent } from "./home/home.component";
 import { PagesComponent } from "./pages.component";
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from "@shared/shared.module";
 
 @NgModule({
   declarations: [HomeComponent, PagesComponent],
